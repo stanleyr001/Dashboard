811c2cffee40ee17167ac8a01c64cb3ef596e4e3
stanleyr001 <stanleyr001@gmail.com>
2015-09-11 12:43:31 -0700

    Added array adapter to display a gridview of flagged messages on the dashbaord.

--- app/src/main/java/com/topicplaces/dashboard/Dashboard.java
+++ app/src/main/java/com/topicplaces/dashboard/Dashboard.java
@@ -1,16 +1,83 @@
 package com.topicplaces.dashboard;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import main.java.SNSController;
 
 public class Dashboard extends AppCompatActivity {
+
+    private GridView dashboardTopicsGridView;
+    private DashboardID[] dashboardIDs;
+
+    private String dashboardTopicTitle = "Dashboard Testing",
+                   dashboardMessageTitle = "Dashboard Permanent Message",
+                   dashTID,
+                   dashGID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        dashboardTopicsGridView = (GridView)findViewById(R.id.dashboard_topics_gridview);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy =
+                    new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        ConnectivityManager cm =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            String USER = "stanleyr001";
+            String PASSWORD = "stanleyr001";
+            String ENDPOINT = "http://tse.topicplaces.com/api/2/";
+            SNSController topicsController = new SNSController(ENDPOINT);
+            String UID = topicsController.verifyUsername(USER);
+            String authKey = topicsController.acquireKey(USER, PASSWORD);
+
+            Map<String, String> privateTopics = topicsController.getPrivateTopicMap(UID);
+            dashTID = privateTopics.get(dashboardTopicTitle);
+
+            Map<String, String> topicMessages = topicsController.getPrivateMessageMap(dashTID, authKey);
+            dashGID = topicMessages.get(dashboardMessageTitle);
+
+            Map<String, String> privateAttributes = topicsController.getAttributes(dashGID, true, authKey);
+
+            String[] dashboardMessageKeys = (String[]) privateAttributes.keySet().toArray();
+            Log.d("DASHBOARDMESSAGEIDS", "DashboardMessageIDs succesfully completed.");
+
+            ArrayAdapter<String> dashboardAdapter = new ArrayAdapter<String>(
+                    getBaseContext(),
+                    R.layout.dashboard_gridview_layout,
+                    R.id.dashboard_topics_gridview,
+                    dashboardMessageKeys);
+
+            dashboardTopicsGridView.setAdapter(dashboardAdapter);
+
+
+
+        }
+
+
+
+
     }
 
     @Override
---
+++ app/src/main/java/com/topicplaces/dashboard/DashboardID.java
@@ -0,0 +1,15 @@
+package com.topicplaces.dashboard;
+
+/**
+ * Created by Stanley on 09/10/15.
+ */
+public class DashboardID {
+
+    private String TID;
+    private String MID;
+
+    DashboardID(String TID, String MID) {
+        this.TID = TID;
+        this.MID = MID;
+    }
+}
